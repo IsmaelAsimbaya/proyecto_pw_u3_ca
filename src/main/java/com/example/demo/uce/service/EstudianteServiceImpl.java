58e122b178c5a44f50b4325b380a60222a7dab30
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2022-08-18 21:52:02 -0500

    Tarea15(18/08/2022)

---
+++ src/main/java/com/example/demo/uce/service/EstudianteServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.uce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.uce.repository.IEstudianteRepository;
+import com.example.demo.uce.repository.modelo.Estudiante;
+
+@Service
+public class EstudianteServiceImpl implements IEstudianteService{
+
+	
+	@Autowired
+	private IEstudianteRepository estudianteRepository;
+	
+	@Override
+	public void crear(Estudiante estudiante) {
+		this.estudianteRepository.crear(estudiante);
+	}
+
+	@Override
+	public void actualizar(Estudiante estudiante) {
+		this.estudianteRepository.actualizar(estudiante);
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		this.estudianteRepository.eliminar(id);
+	}
+
+	@Override
+	public Estudiante buscarPorId(Integer id) {
+		return this.estudianteRepository.buscar(id);
+	}
+
+	
+	
+	
+}
